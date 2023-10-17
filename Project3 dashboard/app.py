7e0f871b6f2b375b9d069776875ebf0aaa19bfac
TJDavis91 <135285282+TJDavis91@users.noreply.github.com>
2023-10-16 20:36:17 -0500

    updated app.py

--- Project3 dashboard/app.py
+++ Project3 dashboard/app.py
@@ -1,20 +1,46 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import psycopg2
-
+import pandas as pd
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/api/data")
-def get_data():
-    conn = psycopg2.connect(database="Currency_db", user= "postgres", password="postgres", host= "localhost, port = '5432")
-    cursor = conn.cursor()
+@app.route("/api/bitcoin")
+def get_bitcoin():
+    conn = psycopg2.connect(database="Currency_db",
+                        user="postgres",
+                        password="postgres",
+                        port="5432")
     query = "SELECT * FROM bitcoin"
-    data = cursor.execute(query).fetchall()
+    data = pd.read_sql(query, conn)
     conn.close()
-    return jsonify(data)
+    return jsonify(data.to_json(orient= "records"))
+
+
+@app.route("/api/gold")
+def get_gold():
+    conn = psycopg2.connect(database="Currency_db",
+                        user="postgres",
+                        password="postgres",
+                        port="5432")
+    query = "SELECT * FROM gold"
+    data = pd.read_sql(query, conn)
+    conn.close()
+    return jsonify(data.to_json(orient= "records"))
+
+
+@app.route("/api/silver")
+def get_silver():
+    conn = psycopg2.connect(database="Currency_db",
+                        user="postgres",
+                        password="postgres",
+                        port="5432")
+    query = "SELECT * FROM silver"
+    data = pd.read_sql(query, conn)
+    conn.close()
+    return jsonify(data.to_json(orient= "records"))
 
 if __name__ == "__main__":
     app.run(debug=True)
