c67f95dbac5ae9a52da9ffd85dade198bedd0b57
TJDavis91 <135285282+TJDavis91@users.noreply.github.com>
2023-10-16 21:09:22 -0500

    app.py additions

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("Project3dashboard.html")
 
 @app.route("/api/bitcoin")
 def get_bitcoin():
